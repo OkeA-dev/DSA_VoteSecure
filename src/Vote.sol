152b6bf5ba35672c0bab14505bd14beac0341b25
Oke.A <Okea.dev@gmail.com>
2024-08-03 09:19:45 +0100

    ...

--- src/Vote.sol
+++ src/Vote.sol
@@ -26,11 +26,13 @@ pragma solidity ^0.8.24;
 import {MerkleProof} from "@openzeppelin/contracts/utils/cryptography/MerkleProof.sol";
 
 contract VoteProtocol {
-    error VoteProtocol__InvalidAddCandidate();
+    error VoteProtocol__InvalidCandidateNameAndAccount();
     error VoteProtocol__InvalidAdmistrator();
     error VoteProtocol__NeedMoreCandidateToRegister();
     error VoteProtocol__InvalidProof();
     error VoteProtocol__AlreadyVoted();
+    error VoteProtocol__RegistrationHasEnded();
+    error VoteProtocol__HasAlreadyRegistered();
 
     enum Status {
         Registration,
@@ -41,6 +43,7 @@ contract VoteProtocol {
     struct Candidate {
         uint256 id;
         string name;
+        address account;
         uint256 voteCount;
     }
 
@@ -48,7 +51,7 @@ contract VoteProtocol {
     bytes32 private immutable i_merkleRoot;
     uint256 private s_candidateCount;
     address public immutable s_voteAdmistrator;
-    mapping(uint256 candidateId => Candidate candidateInformation) public s_candidates;
+    Candidate[] public s_candidates;
     mapping(address voter => bool confirmVote) public s_hasVoted;
 
     event AddCandidate(uint256 candidateCount, string candidateName);
@@ -57,6 +60,7 @@ contract VoteProtocol {
         s_voteAdmistrator = msg.sender;
         i_merkleRoot = merkleRoot;
         currentStatus = Status.Registration;
+        
     }
 
     modifier onlyOwner() {
@@ -65,31 +69,46 @@ contract VoteProtocol {
         }
         _;
     }
+    /**
+     *
+     * @param _name : the name of the candidate to add.
+     * @notice this function allow only the vote Adminstrator to add the new candidate before the voting start.
+     */
 
-    function addCandidate(string memory _name) external onlyOwner {
-        if (bytes(_name).length == 0) {
-            revert VoteProtocol__InvalidAddCandidate();
+    function addCandidate(string memory _name, address account) external onlyOwner {
+        if (bytes(_name).length == 0 && account == address(0)) {
+            revert VoteProtocol__InvalidCandidateNameAndAccount();
         }
-        s_candidates[s_candidateCount] = Candidate({id: s_candidateCount, name: _name, voteCount: 0});
+        if (currentStatus != Status.Registration) {
+            revert VoteProtocol__RegistrationHasEnded();
+        }
+        if (_confirmIfCandidateHasRegistered(account)) {
+            revert VoteProtocol__HasAlreadyRegistered();
+        }
+        uint256 count = 0;
+        s_candidates.push(Candidate({id: count, name: _name, account: account, voteCount: 0}));
 
-        s_candidateCount++;
+        count++;
 
         emit AddCandidate(s_candidateCount, _name);
     }
 
     function startVote() external onlyOwner {
-        if (s_candidateCount > 1) {
+        if (s_candidateCount <= 1) {
             revert VoteProtocol__NeedMoreCandidateToRegister();
         }
         currentStatus = Status.Voting;
-        uint256 endVoting = block.timestamp + 30 minutes;
-
-        if (block.timestamp == endVoting) {
-            _stopVote();
-        }
     }
 
     function countVote() external {}
+
+    /**
+     * 
+     * @param account the account of the voter 
+     * @param candidateId the unique id for the candidate to vote for
+     * @param merkleProof the proof to confirm that you registered for the vote.
+     * @notice this function confirm the authentication of the voter and initial a unchangable vote.
+     */
 
     function vote(address account, uint256 candidateId, bytes32[] calldata merkleProof) external {
         bytes32 leaf = keccak256(bytes.concat(keccak256(abi.encode(account))));
@@ -104,5 +123,18 @@ contract VoteProtocol {
 
     function _stopVote() internal {
         currentStatus = Status.Voting_Ended;
+    }
+
+    function _confirmIfCandidateHasRegistered(address account) internal view returns (bool checked) {
+        for (uint256 i = 0; i < s_candidates.length; i++) {
+            if (s_candidates[i].account == account) {
+                checked = true;
+            }
+        }
+        checked = false;
+    }
+
+    function getCandidateCount() public view returns (uint256 count) {
+        count = s_candidates.length;
     }
 }
--- test/TestVote.t.sol
+++ test/TestVote.t.sol
@@ -28,7 +28,9 @@ import {VoteProtocol} from "../src/Vote.sol";
 
 contract TestVote is Test {
     bytes32 private constant ROOT = 0x826accc591adbdbb3f180343727b312a11c7c5c571e0e0924df1513cbb4c512e;
-    address voterOne;
+    address voter_1;
+    address candidate_1;
+    address candidate_2;
     VoteProtocol vote;
 
     bytes32 proofOne = 0xe5ebd1e1b5a5478a944ecab36a9a954ac3b6b8216875f6524caa7a1d87096576;
@@ -37,12 +39,36 @@ contract TestVote is Test {
 
     function setUp() public {
         vote = new VoteProtocol(ROOT);
-        voterOne = makeAddr("user");
+        voter_1 = makeAddr("user");
+        candidate_1 = makeAddr("candidate1");
+        candidate_2 = makeAddr("candidate2");
     }
 
     function testVoteInvalidCandidate() public {
-        vm.prank(voterOne);
+        vm.prank(voter_1);
         vm.expectRevert();
-        vote.vote(voterOne, 2, PROOF );
+        vote.vote(voter_1, 2, PROOF);
+    }
+
+    /////////////////////////////
+    //    ADD CANDIDATE       //
+    ///////////////////////////
+
+    function testOnlyVoteAdmistratorCanAddCandidate() public {
+        vm.prank(voter_1);
+        vm.expectRevert(VoteProtocol.VoteProtocol__InvalidAdmistrator.selector);
+        vote.addCandidate("Oke Abdulquadri", candidate_1);
+    }
+
+    function testAddInvalidCandidate() public {
+        vm.expectRevert(VoteProtocol.VoteProtocol__InvalidCandidateNameAndAccount.selector);
+        vote.addCandidate("", address(0));
+    }
+
+    function testAddCandidate() public {
+        uint256 count = 2;
+        vote.addCandidate("Oke Abdulquadri", candidate_1);
+        vote.addCandidate("Adu Samson", candidate_2);
+        assertEq(vote.getCandidateCount(), count);
     }
 }
